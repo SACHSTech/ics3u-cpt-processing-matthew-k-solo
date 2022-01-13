3388c054c092a192d6975d58787c29b1f2412dfc
John Matthew Kassapian <johnmatthew.kassapian23@ycdsbk12.ca>
2022-01-13 13:05:17 -0500

    sussy

--- Sketch.java
+++ Sketch.java
@@ -2,13 +2,14 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
+  float[] speed = new float[2];
+  float[] ballCoords = new float[2];	
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(400, 600);
   }
 
   /** 
@@ -16,21 +17,50 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(215,255,194);
+    ballCoords[0] = width / 2;	
+    ballCoords[1] = (float) (height / 1.25);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+	 
+    //grass pattern
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    for(int i = 0; i < width; i += (width / 5)){
+      for(int j = 0; j < height; j += (height / 10)){
+        noStroke();
+        fill(60,106,73);
+        rect(i, j, width / 10, height / 20);
+      }
+
+    }
+
+    for(int i = width / 10; i < width; i += (width / 5)){
+      for(int j = height / 20; j < height; j += (height / 10)){
+        noStroke();
+        fill(60,106,73);
+        rect(i, j, width / 10, height / 20);
+      }
+
+    }
+
+    // blocks
+
+    // hole
+
+    // golf ball
+    strokeWeight(2);
+    stroke(0);
+    fill(255);
+    ellipse(ballCoords[0], ballCoords[1], 20, 20);
+
+  }
+
+  public void mouseDragged(){
+    
   }
   
-  // define other methods down here.
 }
